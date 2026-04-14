f472101d1dc178838e3b0c1274b341ccaa9312db
Vaughan <39153417+vaughanthompson@users.noreply.github.com>
2026-04-15 09:33:57 +1200

    1st push

---
+++ js/site.js
@@ -0,0 +1,54 @@
+﻿    $(document).ready(function(){
+        $.localScroll({
+                target: 'body',
+                queue: false,
+                hash: false,
+                duration: 500,
+                easing: 'easeOutQuad'
+        });
+
+
+
+        $('a#toTop').css('opacity','0');
+        new Waypoint({
+        element: document.getElementsByTagName('main'),
+        handler: function(direction) {
+            if (direction === 'down') {
+                $('a#toTop').fadeTo(1000,'1.0');
+            }
+            else {
+                $('a#toTop').fadeTo(1000,'0');
+            }
+        },
+            offset: '-150%'
+        })
+
+      
+
+        // Mobile Navigation
+        $('#navClose').hide();
+
+        $('#navClose').click(function(){
+            $(this).hide();
+            $('#navCart').show();
+            $('#navTrigger').show();
+            $("div.mblnavContainer").scrollTo(0);
+            $('div.mblnav').hide();
+            $('html, body').removeClass('noscroll');
+            return false;
+        });
+
+        $('#navTrigger').click(function(){
+            $(this).hide();
+            $('#navCart').hide();
+            $('#navClose').show();
+            $('div.mblnav').fadeTo(400,1);
+            $('html, body').addClass('noscroll');
+            return false;
+
+        });
+
+
+
+
+    }); 
